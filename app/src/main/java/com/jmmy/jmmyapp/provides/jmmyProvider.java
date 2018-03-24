ad691c2b651b7af053346398633c80c1654d1ba4
jmmy <jm_my2012@163.com>
2018-03-24 19:13:54 +0800

    修改ContentProvider

--- app/src/main/java/com/jmmy/jmmyapp/provides/jmmyProvider.java
+++ app/src/main/java/com/jmmy/jmmyapp/provides/jmmyProvider.java
@@ -1,13 +1,48 @@
 package com.jmmy.jmmyapp.provides;
 
-import java.security.Provider;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 /**
  * Created by jmmy on 2017/12/15.
  */
 
-public class jmmyProvider extends Provider {
-    protected jmmyProvider(String name, double version, String info) {
-        super(name, version, info);
+public class jmmyProvider extends ContentProvider {
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
     }
 }
