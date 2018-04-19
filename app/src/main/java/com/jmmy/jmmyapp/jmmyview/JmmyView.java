ef1bc91089eaeab2ffa582a05c10af26d30dbd1d
jmmy <jm_my2012@163.com>
2018-04-19 22:06:19 +0800

    Mac2018年4月19日提交

--- app/src/main/java/com/jmmy/jmmyapp/jmmyview/JmmyView.java
+++ app/src/main/java/com/jmmy/jmmyapp/jmmyview/JmmyView.java
@@ -2,20 +2,11 @@ package com.jmmy.jmmyapp.jmmyview;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.util.AttributeSet;
 import android.view.View;
 
 public class JmmyView extends View {
     public JmmyView(Context context) {
         super(context);
-    }
-
-    public JmmyView(Context context, @android.support.annotation.Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public JmmyView(Context context, @android.support.annotation.Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
     }
 
     @Override
