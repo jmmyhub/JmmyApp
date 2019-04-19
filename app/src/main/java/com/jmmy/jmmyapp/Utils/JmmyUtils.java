b54f52f2e5b6c2c6e44ad044cb2649881bdcb61d
jmmy <jm_my2012@163.com>
2019-04-19 21:27:08 +0800

    全部工程文件提交

--- app/src/main/java/com/jmmy/jmmyapp/Utils/JmmyUtils.java
+++ app/src/main/java/com/jmmy/jmmyapp/Utils/JmmyUtils.java
@@ -1,8 +1,24 @@
 package com.jmmy.jmmyapp.Utils;
 
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by jmmy on 2017/12/15.
  */
 
 public class JmmyUtils {
+    private ServerSocket serverSocket = null;
+      public void getSocket (){
+          try {
+              serverSocket = new ServerSocket(1000);
+              Socket socket = serverSocket.accept();
+              socket.getInputStream();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+      }
+
 }
